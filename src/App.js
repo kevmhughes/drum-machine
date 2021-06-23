f151cd54513febaa23492234b1e5194ce8132fe3
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-06-23 02:48:08 +0200

    Add drum-pad component, and refactor code

--- src/App.js
+++ src/App.js
@@ -1,143 +1,91 @@
 import React, { Component } from 'react';
+import DrumPad from './components/drum-pad/drum-pad';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      padName: "",
+      display: "Hit a pad!",
+      currentPadBank: [this.data],
     };
   }
 
-  playAudio() {
-    window.addEventListener('keypress', e => {
-      if (e.keyCode === 113) { audio1.play() }
-      audio1.currentTime = 0;
-    });
-    const audio1 = document.getElementById("Q")
-    audio1.play()
-    audio1.currentTime = 0;
-  }
+  data = [
+    {
+      keyCode: 81,
+      keyTrigger: 'Q',
+      id: 'Heater-1',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    },
+    {
+      keyCode: 87,
+      keyTrigger: 'W',
+      id: 'Heater-2',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    },
+    {
+      keyCode: 69,
+      keyTrigger: 'E',
+      id: 'Heater-3',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    },
+    {
+      keyCode: 65,
+      keyTrigger: 'A',
+      id: 'Heater-4',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    },
+    {
+      keyCode: 83,
+      keyTrigger: 'S',
+      id: 'Clap',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    },
+    {
+      keyCode: 68,
+      keyTrigger: 'D',
+      id: 'Open-HH',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    },
+    {
+      keyCode: 90,
+      keyTrigger: 'Z',
+      id: "Kick-n'-Hat",
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    },
+    {
+      keyCode: 88,
+      keyTrigger: 'X',
+      id: 'Kick',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    },
+    {
+      keyCode: 67,
+      keyTrigger: 'C',
+      id: 'Closed-HH',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    }
+  ];
 
-  playAudioTwo() {
-    window.addEventListener('keypress', e => {
-      if (e.keyCode === 119) { audio2.play() }
-      audio2.currentTime = 0;
-    });
-    const audio2 = document.getElementById("W")
-    audio2.currentTime = 0;
-    audio2.play()
-  }
-
-  playAudioThree() {
-    window.addEventListener('keypress', e => {
-      if (e.keyCode === 101) { audio3.play() }
-      audio3.currentTime = 0;
-    });
-    const audio3 = document.getElementById("E")
-    audio3.currentTime = 0;
-    audio3.play()
-  }
-
-  playAudioFour() {
-    window.addEventListener('keypress', e => {
-      if (e.keyCode === 97) { audio4.play() }
-      audio4.currentTime = 0;
-    });
-    const audio4 = document.getElementById("A")
-    audio4.currentTime = 0;
-    audio4.play()
-  }
-  playAudioFive() {
-    window.addEventListener('keypress', e => {
-      if (e.keyCode === 115) { audio5.play() }
-      audio5.currentTime = 0;
-    });
-    const audio5 = document.getElementById("S")
-    audio5.currentTime = 0;
-    audio5.play()
-  }
-  playAudioSix() {
-    window.addEventListener('keypress', e => {
-      if (e.keyCode === 100) { audio6.play() }
-      audio6.currentTime = 0;
-    });
-    const audio6 = document.getElementById("D")
-    audio6.currentTime = 0;
-    audio6.play()
-  }
-  playAudioSeven() {
-    window.addEventListener('keypress', e => {
-      if (e.keyCode === 122) { audio7.play() }
-      audio7.currentTime = 0;
-    });
-    const audio7 = document.getElementById("Z")
-    audio7.currentTime = 0;
-    audio7.play()
-  }
-  playAudioEight() {
-    window.addEventListener('keypress', e => {
-      if (e.keyCode === 120) { audio8.play() }
-      audio8.currentTime = 0;
-    });
-    const audio8 = document.getElementById("X")
-    audio8.currentTime = 0;
-    audio8.play()
-  }
-  playAudioNine() {
-    window.addEventListener('keypress', e => {
-      if (e.keyCode === 99) { audio9.play() }
-      audio9.currentTime = 0;
-    });
-    const audio9 = document.getElementById("C")
-    audio9.currentTime = 0;
-    audio9.play()
-  }
-
+  handleDisplay = display => this.setState({ display })
 
 
   render() {
     return(
       <div id="drum-machine">
-        <div id="display"></div>
-        <div id="pads">
-          <button onClick={this.playAudio} id="sound-one" className="drum-pad">Q
-                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" type="audio/wav" className="clip" id="Q">
-                </audio>
-          </button>
-        <button onClick={this.playAudioTwo} id="sound-two" className="drum-pad">W
-            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" type="audio/wav" className="clip" id="W">
-            </audio>
-        </button>
-        <button onClick={this.playAudioThree} id="sound-three" className="drum-pad">E
-            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" type="audio/wav" className="clip" id="E">
-            </audio>
-        </button>
-        <button onClick={this.playAudioFour} id="sound-four" className="drum-pad">A
-            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" type="audio/wav" className="clip" id="A">
-            </audio>
-        </button>
-        <button onClick={this.playAudioFive} id="sound-five" className="drum-pad">S
-            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" type="audio/wav" className="clip" id="S">
-            </audio>
-        </button>
-        <button onClick={this.playAudioSix} id="sound-six" className="drum-pad">D
-            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" type="audio/wav" className="clip" id="D">
-            </audio>
-        </button>
-        <button onClick={this.playAudioSeven} id="sound-seven" className="drum-pad">Z
-            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" type="audio/wav" className="clip" id="Z">
-            </audio>
-        </button>
-        <button onClick={this.playAudioEight} id="sound-eight" className="drum-pad">X
-            <audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" type="audio/wav" className="clip" id="X">
-            </audio>
-        </button>
-        <button onClick={this.playAudioNine} id="sound-nine" className="drum-pad">C
-            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" type="audio/wav" className="clip" id="C">
-            </audio>
-        </button>
-        </div>
+        <div id="display">{this.state.display}</div>
+          <div id="drum-layout">
+            {this.data.map(d => ( 
+              <DrumPad 
+                id={d.id}
+                letter={d.keyTrigger}
+                src={d.url}
+                key={d.keyCode}
+                handleDisplay={this.handleDisplay}
+              />
+            ))}
+          </div>
       </div>
     )
   }
---
+++ src/components/drum-pad/drum-pad.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export default class DrumPad extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+    window.focus()
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+
+  handleKeyDown = e => {
+    if(e.keyCode === this.props.letter.charCodeAt()) {
+      this.audio.play()
+      this.audio.currentTime = 0;
+      this.props.handleDisplay(this.props.id)
+    }
+  }
+
+  handleClick = () => {
+    this.audio.play()
+    this.audio.currentTime = 0;
+    this.props.handleDisplay(this.props.id)
+  }
+
+  render() {
+    return(
+        <button 
+          className="drum-pad" 
+          onClick={this.handleClick}
+          id={this.props.id}>
+          <h1>{this.props.letter}</h1>
+            <audio 
+              ref={ref => this.audio = ref}
+              className="clip"
+              src={this.props.src} 
+              id={this.props.letter}
+            >
+            </audio>
+        </button>
+    );
+  }
+}
