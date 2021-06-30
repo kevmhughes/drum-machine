add8d14b34469d691a2e33f35fe30d567c50a406
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-07-01 01:21:43 +0200

    Add ternary operators to change pad styling on press

--- src/components/drum-pad/drum-pad.jsx
+++ src/components/drum-pad/drum-pad.jsx
@@ -5,9 +5,12 @@ export default class DrumPad extends Component {
   constructor(props){
     super(props);
     this.state = {
+      buttonPress: true,
     };
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handlePress = this.handlePress.bind(this);
+    this.handleRelease = this.handleRelease.bind(this);
   }
 
   componentDidMount() {
@@ -24,6 +27,14 @@ export default class DrumPad extends Component {
       this.audio.play()
       this.audio.currentTime = 0;
       this.props.handleDisplay(this.props.id)
+      this.setState({buttonPress: !this.state.buttonPress})
+      setTimeout(
+        function() {
+            this.setState({buttonPress: !this.state.buttonPress});
+        }
+        .bind(this),
+        100
+    );
     }
   }
 
@@ -33,13 +44,24 @@ export default class DrumPad extends Component {
     this.props.handleDisplay(this.props.id)
   }
 
+  handlePress = () => {
+    this.setState({buttonPress: !this.state.buttonPress})
+  }
+
+  handleRelease = () => {
+    this.setState({buttonPress: !this.state.buttonPress})
+  }
+
   render() {
+    const btnClassOnPress = this.state.buttonPress ? "not-pressed" : "pressed"
     return(
         <button 
           className="drum-pad linear" 
-          onClick={this.handleClick}
+          onClick={ this.handleClick }
+          onMouseDown={ this.handlePress }
+          onMouseUp={ this.handleRelease }
           id={this.props.id}>
-          <h1>{this.props.letter}</h1>
+          <h1 className={btnClassOnPress}>{this.props.letter}</h1>
             <audio 
               ref={ref => this.audio = ref}
               className="clip"
