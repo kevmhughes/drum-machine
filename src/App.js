df60436bfecdbc0b4c7ac30df6a670206d4998c1
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-07-01 03:08:55 +0200

    Lint with ESLint

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-use-before-define */
 import React, { Component } from 'react';
-import NinePads from './components/nine-pads/nine-pads';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faDrum, faVolumeDown, faVolumeUp,
 } from '@fortawesome/free-solid-svg-icons';
+import NinePads from './components/nine-pads/nine-pads';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      display: "Hit a pad!",
+      display: 'Hit a pad!',
       switch: true,
       sliderVal: 0.3,
     };
@@ -19,76 +24,91 @@ class App extends Component {
   }
 
   handleSwitch() {
-    this.setState(prevState => ({
-      switch: !prevState.switch
+    this.setState((prevState) => ({
+      switch: !prevState.switch,
     }));
   }
 
-  handleDisplay = display => this.setState({ display })
+  handleDisplay = (display) => this.setState({ display })
 
   adjustVolume(e) {
-      this.setState({
-        sliderVal: e.target.value,
-        display: 'Volume: ' + Math.round(e.target.value * 100)
-      });
-      setTimeout(() => this.clearDisplay(), 1000);
+    this.setState({
+      sliderVal: e.target.value,
+      display: `Volume: ${Math.round(e.target.value * 100)}`,
+    });
+    setTimeout(() => this.clearDisplay(), 1000);
   }
 
   clearDisplay() {
     this.setState({
-      display: ''
+      display: '',
     });
   }
 
   render() {
     {
       const clips = [].slice.call(document.getElementsByClassName('clip'));
-      clips.forEach(sound => {
+      clips.forEach((sound) => {
         sound.volume = this.state.sliderVal;
       });
     }
-    return(
+    return (
       <div id="drum-machine">
-        <div className="top"></div>
+        <div className="top" />
         <div id="display-panel">
           <div id="display">
             {this.state.display}
           </div>
-          {this.state.switch ? <div id="display-two">
-          Kit 1
-          </div> : <div id="display-two">
-          Kit 2
-          </div> }
+          {this.state.switch ? (
+            <div id="display-two">
+              Kit 1
+            </div>
+          ) : (
+            <div id="display-two">
+              Kit 2
+            </div>
+          ) }
         </div>
         <div className="control-panel">
           <div style={{ display: 'flex' }}>
-              <FontAwesomeIcon icon={faVolumeDown} style={{ marginRight: '5px',
-    color:'darkgray' }}/>
-              <input
-                  className="slider"
-                  max='1'
-                  min='0'
-                  onChange={this.adjustVolume}
-                  step='0.01'
-                  type='range'
-                  value={this.state.sliderVal}
-                />
-                <FontAwesomeIcon icon={faVolumeUp} style={{ marginLeft: '5px',
-    color:'darkgray' }}/>
-            </div>
-          <button 
-            onClick={this.handleSwitch} 
+            <FontAwesomeIcon
+              icon={faVolumeDown}
+              style={{
+                marginRight: '5px',
+                color: 'darkgray',
+              }}
+            />
+            <input
+              className="slider"
+              max="1"
+              min="0"
+              onChange={this.adjustVolume}
+              step="0.01"
+              type="range"
+              value={this.state.sliderVal}
+            />
+            <FontAwesomeIcon
+              icon={faVolumeUp}
+              style={{
+                marginLeft: '5px',
+                color: 'darkgray',
+              }}
+            />
+          </div>
+          <button
+            onClick={this.handleSwitch}
             id="change-pad-button"
           >
-            <FontAwesomeIcon icon={faDrum} style={{ fontSize:'28px' }}/>
+            <FontAwesomeIcon icon={faDrum} style={{ fontSize: '28px' }} />
           </button>
         </div>
-        <NinePads 
+        <NinePads
           clipVolume={this.state.sliderVal}
           switch={this.state.switch}
-          handleDisplay={this.handleDisplay}/>
+          handleDisplay={this.handleDisplay}
+        />
       </div>
-    )
+    );
   }
 }
 
--- src/components/drum-pad/drum-pad.jsx
+++ src/components/drum-pad/drum-pad.jsx
@@ -1,8 +1,15 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/media-has-caption */
+/* eslint-disable no-return-assign */
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-use-before-define */
 import React, { Component } from 'react';
 import './drum-pad.css';
 
 export default class DrumPad extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       buttonPress: true,
@@ -15,61 +22,60 @@ export default class DrumPad extends Component {
 
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyDown);
-    window.focus()
+    window.focus();
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown)
+    document.removeEventListener('keydown', this.handleKeyDown);
   }
 
-  handleKeyDown = e => {
-    if(e.keyCode === this.props.letter.charCodeAt()) {
-      this.audio.play()
+  handleKeyDown = (e) => {
+    if (e.keyCode === this.props.letter.charCodeAt()) {
+      this.audio.play();
       this.audio.currentTime = 0;
-      this.props.handleDisplay(this.props.id)
-      this.setState({buttonPress: !this.state.buttonPress})
+      this.props.handleDisplay(this.props.id);
+      this.setState({ buttonPress: !this.state.buttonPress });
       setTimeout(
-        function() {
-            this.setState({buttonPress: !this.state.buttonPress});
-        }
-        .bind(this),
-        100
-    );
+        () => {
+          this.setState({ buttonPress: !this.state.buttonPress });
+        },
+        100,
+      );
     }
   }
 
   handleClick = () => {
-    this.audio.play()
+    this.audio.play();
     this.audio.currentTime = 0;
-    this.props.handleDisplay(this.props.id)
+    this.props.handleDisplay(this.props.id);
   }
 
   handlePress = () => {
-    this.setState({buttonPress: !this.state.buttonPress})
+    this.setState({ buttonPress: !this.state.buttonPress });
   }
 
   handleRelease = () => {
-    this.setState({buttonPress: !this.state.buttonPress})
+    this.setState({ buttonPress: !this.state.buttonPress });
   }
 
   render() {
-    const btnClassOnPress = this.state.buttonPress ? "not-pressed" : "pressed"
-    return(
-        <button 
-          className="drum-pad linear" 
-          onClick={ this.handleClick }
-          onMouseDown={ this.handlePress }
-          onMouseUp={ this.handleRelease }
-          id={this.props.id}>
-          <h1 className={btnClassOnPress}>{this.props.letter}</h1>
-            <audio 
-              ref={ref => this.audio = ref}
-              className="clip"
-              src={this.props.src} 
-              id={this.props.letter}
-            >
-            </audio>
-        </button>
+    const btnClassOnPress = this.state.buttonPress ? 'not-pressed' : 'pressed';
+    return (
+      <button
+        className="drum-pad linear"
+        onClick={this.handleClick}
+        onMouseDown={this.handlePress}
+        onMouseUp={this.handleRelease}
+        id={this.props.id}
+      >
+        <h1 className={btnClassOnPress}>{this.props.letter}</h1>
+        <audio
+          ref={(ref) => this.audio = ref}
+          className="clip"
+          src={this.props.src}
+          id={this.props.letter}
+        />
+      </button>
     );
   }
 }
